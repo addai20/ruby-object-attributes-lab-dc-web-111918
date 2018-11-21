d05258eda2896423a24c080d942e7724e5ddc5ae
addai.5 <addai.5@buckeyemail.osu.edu>
2018-11-21 20:06:27 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog 
+  
+  
+  def name=(name)
+    @name = name
+  end
+  
+  def name(name)
+    puts @name
+  end
+  
+  def breed=(breed) 
+    @breed = breed
+  end
+  
+  def breed()
+    puts Dog.breed
+  end
+  
+  
+end
+
